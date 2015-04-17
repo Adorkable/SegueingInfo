14e832364b8d0286049d4f88ed25f4e246908541
Ian Grossberg <yo.ian.g@gmail.com>
2015-04-17 00:38:58 -0400

    Force Swift Umbrella creation for Framework

---
+++ SegueingInfo/SegueingInfoFrameworkiOS-Force-Umbrella.swift
@@ -0,0 +1,9 @@
+//
+//  SegueingInfoFrameworkiOS-Force-Umbrella.swift
+//  SegueingInfo
+//
+//  Created by Ian on 4/17/15.
+//  Copyright (c) 2015 Adorkable. All rights reserved.
+//
+
+import Foundation

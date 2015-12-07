8afb72179c9c0305187797ac6bed74819d8ac148
Ian G <yo.ian.g@gmail.com>
2015-12-06 23:46:02 -0500

    updated generateChangelog to fit with Shellcheck

--- Tools/generateChangelog.sh
+++ Tools/generateChangelog.sh
@@ -3,7 +3,10 @@
 github_changelog_generator --no-verbose
 
 checkFileUncommitted() {
-	if git status --porcelain | grep -q $1; then
+	git status --porcelain | grep -q "$1"
+	result=$?
+	if [ "$result" = "0" ] 
+	then
 		echo "* $1 is dirty"
 	    return 1
 	else
